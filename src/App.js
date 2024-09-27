b664918d3adcf21cce17a92d44d978ee78bea05b
Simran Meena <simranmeena2004@gmail.com>
2024-09-27 16:49:34 +0530

    Navbar done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
-import Home from './pages/Home';
 import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Team from './pages/Team';
+import Projects from './pages/Projects';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Home/>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,205 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <>
+      <nav className="bg-gray-800 sticky top-0 z-50">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="relative flex h-16 items-center justify-between">
+            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              <button
+                type="button"
+                className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                aria-controls="mobile-menu"
+                aria-expanded="false"
+              >
+                <span className="absolute -inset-0.5"></span>
+                <span className="sr-only">Open main menu</span>
+                <svg
+                  className="block h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                  />
+                </svg>
+                <svg
+                  className="hidden h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </div>
+            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+              <div className="flex flex-shrink-0 items-center">
+                <img
+                  className="h-8 w-auto"
+                  src="https://images.squarespace-cdn.com/content/v1/623126b50d2dfb281caaafd3/29f801c9-e057-4041-831f-4729e98e598c/The-Ohio-State-University-logo.jpg"
+                  alt="Your Company"
+                />
+              </div>
+              <div className="hidden sm:ml-6 sm:block">
+                <div className="flex space-x-4">
+                  <NavLink
+                    to="/"
+                    className={({ isActive }) =>
+                      `rounded-md px-3 py-2 text-sm font-medium ${
+                        isActive ? "bg-gray-600 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white"
+                      }`
+                    }
+                    end
+                  >
+                    Home
+                  </NavLink>
+                  <NavLink
+                    to="team"
+                    className={({ isActive }) =>
+                      `rounded-md px-3 py-2 text-sm font-medium ${
+                        isActive ? "bg-gray-600 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white"
+                      }`
+                    }
+                  >
+                    Team
+                  </NavLink>
+                  <NavLink
+                    to="projects"
+                    className={({ isActive }) =>
+                      `rounded-md px-3 py-2 text-sm font-medium ${
+                        isActive ? "bg-gray-600 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white"
+                      }`
+                    }
+                  >
+                    Projects
+                  </NavLink>
+                  <NavLink
+                    to="calendar"
+                    className={({ isActive }) =>
+                      `rounded-md px-3 py-2 text-sm font-medium ${
+                        isActive ? "bg-gray-600 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white"
+                      }`
+                    }
+                  >
+                    Calendar
+                  </NavLink>
+                </div>
+              </div>
+            </div>
+            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              <button
+                type="button"
+                className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+              >
+                <span className="absolute -inset-1.5"></span>
+                <span className="sr-only">View notifications</span>
+                <svg
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
+                  />
+                </svg>
+              </button>
+              <div className="relative ml-3">
+                <div>
+                  <button
+                    type="button"
+                    className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                    id="user-menu-button"
+                    aria-expanded="false"
+                    aria-haspopup="true"
+                  >
+                    <span className="absolute -inset-1.5"></span>
+                    <span className="sr-only">Open user menu</span>
+                    <div className="relative w-8 h-8 overflow-hidden bg-gray-600 rounded-full dark:bg-gray-600">
+                      <svg
+                        className="absolute w-10 h-10 text-gray-400 -left-1"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                          clipRule="evenodd"
+                        ></path>
+                      </svg>
+                    </div>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="sm:hidden" id="mobile-menu">
+          <div className="space-y-1 px-2 pb-3 pt-2">
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                `block rounded-md px-3 py-2 text-base font-medium ${
+                  isActive ? "bg-gray-600 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white"
+                }`
+              }
+              aria-current="page"
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="team"
+              className={({ isActive }) =>
+                `block rounded-md px-3 py-2 text-base font-medium ${
+                  isActive ? "bg-gray-600 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white"
+                }`
+              }
+            >
+              Team
+            </NavLink>
+            <NavLink
+              to="projects"
+              className={({ isActive }) =>
+                `block rounded-md px-3 py-2 text-base font-medium ${
+                  isActive ? "bg-gray-600 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white"
+                }`
+              }
+            >
+              Projects
+            </NavLink>
+            <NavLink
+              to="calendar"
+              className={({ isActive }) =>
+                `block rounded-md px-3 py-2 text-base font-medium ${
+                  isActive ? "bg-gray-600 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white"
+                }`
+              }
+            >
+              Calendar
+            </NavLink>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
