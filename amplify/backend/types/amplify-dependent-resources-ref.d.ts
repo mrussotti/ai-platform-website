6bf5a7efa03194365dc4b04bb58a61878e106271
mrussotti <matthewlax24@yahoo.com>
2024-12-11 09:56:02 -0500

    added git ignore and removed file

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -4,6 +4,11 @@ export type AmplifyDependentResourcesAttributes = {
       "ApiId": "string",
       "ApiName": "string",
       "RootUrl": "string"
+    },
+    "aiPlatformApiProd": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
     }
   },
   "function": {
